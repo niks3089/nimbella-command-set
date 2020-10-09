340c0b146605e780454585ffc1a8adb40fcd7a24
Nikhil Acharya <niks3089@gmail.com>
2020-10-09 21:30:46 +0530

    Nimbella command set

---
+++ README.md
@@ -0,0 +1,46 @@
+# nimbella Command Set
+
+## Commands
+
+- [`action_list`](#action_list)
+- [`object_list`](#object_list)
+- [`package_list`](#package_list)
+- [`redis_list`](#redis_list)
+- [`web_list`](#web_list)
+
+## Usage
+
+### `action_list`
+
+List actions in your namespace
+```sh
+/nc action_list
+```
+
+### `object_list`
+
+List object content in your namespace
+```sh
+/nc object_list
+```
+
+### `package_list`
+
+List packages in your namespace
+```sh
+/nc package_list
+```
+
+### `redis_list`
+
+List key-value content in your namespace
+```sh
+/nc redis_list
+```
+
+### `web_list`
+
+List web content in your namespace
+```sh
+/nc web_list
+```
---
+++ packages/nimbella/object_list/index.js
@@ -0,0 +1,51 @@
+// jshint esversion: 9
+const { runNimCommand} = require('nimbella-cli');
+const nimDeployer = require('nimbella-cli/lib/deployer');
+
+async function loginAuth(auth) {
+  await runNimCommand('auth/login', ['--auth=' + auth]);
+}
+
+/**
+ * @description Run the user command
+ * @param {ParamsType} params list of command parameters
+ * @param {?string} commandText text message
+ * @param {!object} [secrets = {}] list of secrets
+ * @return {Promise<SlackBodyType>} Response body
+ */
+async function _command(params, commandText, secrets = {}) {
+  let res;
+
+  try {
+    await loginAuth(secrets.auth);
+    nimDeployer.initializeAPI("nimbella-cli");
+    res = await runNimCommand('object/list', ['--namespace=' + secrets.namespace]);
+    res = res.table;
+  } catch (e) {
+    console.log('getWeb Error:', e.message);
+    res = e.message;
+  }
+  return {
+    response_type: 'in_channel', // or `ephemeral` for private response
+    text: res
+  };
+}
+
+/**
+ * @typedef {object} SlackBodyType
+ * @property {string} text
+ * @property {'in_channel'|'ephemeral'} [response_type]
+ */
+
+const main = async args => ({
+  body: await _command(
+    args.params,
+    args.commandText,
+    args.__secrets || {}
+  ).catch(error => ({
+    // To get more info, run `/nc activation_log` after your command executes
+    response_type: 'ephemeral',
+    text: `Error: ${error.message}`,
+  })),
+});
+module.exports = main;
